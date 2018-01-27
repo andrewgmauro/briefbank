fc258681fe651b2dfbba133caf40e61666f38325
andrewgmauro <andrewgmauro@gmail.com>
2018-01-27 11:29:15 -0500

    new file:   bank/.babelrc 	new file:   bank/.editorconfig 	new file:   bank/.gitignore 	new file:   bank/.postcssrc.js 	new file:   bank/README.md 	new file:   bank/build/build.js 	new file:   bank/build/check-versions.js 	new file:   bank/build/logo.png 	new file:   bank/build/utils.js 	new file:   bank/build/vue-loader.conf.js 	new file:   bank/build/webpack.base.conf.js 	new file:   bank/build/webpack.dev.conf.js 	new file:   bank/build/webpack.prod.conf.js 	new file:   bank/config/dev.env.js 	new file:   bank/config/index.js 	new file:   bank/config/prod.env.js 	new file:   bank/index.html 	new file:   bank/package-lock.json 	new file:   bank/package.json 	new file:   bank/src/App.vue 	new file:   bank/src/assets/logo.png 	new file:   bank/src/components/home.vue 	new file:   bank/src/components/results.vue 	new file:   bank/src/database.js 	new file:   bank/src/main.js 	new file:   bank/static/.gitkeep 	new file:   package-lock.json

---
+++ bank/src/components/home.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="home">
+        <h1>Brief Bank</h1>
+        <input type="text" v-model="message" placeholder="Search Terms"><br>
+        <input type="text" v-model="message" placeholder="Date"></br>
+        <button type="button">Search:</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Home'
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ bank/src/database.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var app = express();
+
+
+
+
+
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "07002!Perseus",
+    database: "briefbank"
+});
+
+
+var name
+
+var date = 
+
+
+
+connection.end()
+
